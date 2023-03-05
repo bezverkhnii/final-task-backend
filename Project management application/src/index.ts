f426f31d430c8807269c207c35e1a62e617bc6be
Rostyslav Bezverkhnii <mr.strong.bezverkhniy7@gmail.com>
2023-03-05 14:47:10 +0100

    first commit

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://bezverkhnii:<password>@cluster0.dlzajub.mongodb.net/?retryWrites=true&w=majority.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
